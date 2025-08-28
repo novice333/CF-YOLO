f1e03fe782317d0a9481a40f08ecaa0b8dd60d62
novice333 <120295307+novice333@users.noreply.github.com>
2025-08-28 18:08:07 +0800

    train.py

--- yolov11-main/train.py
+++ yolov11-main/train.py
@@ -4,12 +4,8 @@ warnings.filterwarnings('ignore')
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO('yolo11s_attn3_cbamlg.yaml')
-    # 如何切换模型版本, 上面的ymal文件可以改为 yolov11s.yaml就是使用的v11s,
-    # 类似某个改进的yaml文件名称为yolov11-XXX.yaml那么如果想使用其它版本就把上面的名称改为yolov11l-XXX.yaml即可（改的是上面YOLO中间的名字不是配置文件的）！
-    # model.load('yolov11n.pt') # 是否加载预训练权重,科研不建议大家加载否则很难提升精度
+    model = YOLO('yolo11s_all4.yaml')
     model.train(data=r"data/my_VisDrone.yaml",
-                # 如果大家任务是其它的'ultralytics/cfg/default.yaml'找到这里修改task可以改成detect, segment, classify, pose
                 cache=False,
                 imgsz=640,
                 epochs=100,
